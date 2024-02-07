632ab2b339026d7e30eff0cb4f61aff51b8794fb
alina-vakulenko <alina.vakulenko12@gmail.com>
2024-02-07 20:16:23 +0200

    handled ai stream response

--- src/components/chat/ChatContext.tsx
+++ src/components/chat/ChatContext.tsx
@@ -100,6 +100,84 @@ export const ChatContextProvider = ({
           previousMessages?.pages.flatMap((page) => page.messages) || [],
       };
     },
+    onSuccess: async (stream) => {
+      setIsLoading(false);
+
+      if (!stream) {
+        return toast({
+          title: "There was a problem sending this message",
+          description: "Please refresh this page and try again",
+          variant: "destructive",
+        });
+      }
+
+      const reader = stream.getReader();
+      const decoder = new TextDecoder();
+
+      let done = false;
+
+      let accumResponse = "";
+
+      while (!done) {
+        const { value, done: doneReading } = await reader.read();
+        done = doneReading;
+
+        const chunkValue = decoder.decode(value);
+
+        accumResponse += chunkValue;
+
+        utils.getFileMessages.setInfiniteData(
+          { fileId, limit: INFINITE_QUERY_LIMIT },
+          (old) => {
+            if (!old) {
+              return {
+                pages: [],
+                pageParams: [],
+              };
+            }
+
+            let isAiResponseCreated = old.pages.some((page) =>
+              page.messages.some((msg) => msg.id === "ai-response")
+            );
+
+            let updatedPages = old.pages.map((page) => {
+              if (page === old.pages[0]) {
+                let updatedMessages;
+
+                if (!isAiResponseCreated) {
+                  updatedMessages = [
+                    {
+                      createdAt: new Date().toISOString(),
+                      id: "ai-response",
+                      text: accumResponse,
+                      isUserMessage: false,
+                    },
+                    ...page.messages,
+                  ];
+                } else {
+                  updatedMessages = page.messages.map((msg) => {
+                    if (msg.id === "ai-response") {
+                      return { ...msg, text: accumResponse };
+                    }
+                    return msg;
+                  });
+                }
+                return {
+                  ...page,
+                  messages: updatedMessages,
+                };
+              }
+              return page;
+            });
+
+            return {
+              ...old,
+              pages: updatedPages,
+            };
+          }
+        );
+      }
+    },
     onError: (_, _, context) => {
       setMessage(backupMessage.current);
       utils.getFileMessages.setData(
